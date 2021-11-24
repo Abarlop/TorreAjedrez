c8cde068d0f4ce1e6d6f2e57e7c935355a5ca099
Abarlop <85289915+Abarlop@users.noreply.github.com>
2021-11-24 19:43:30 +0100

    Posicion Getter y setter y comprobacion
    Creamos los getter y los setters asai como  hacemos las comprobaciones necesarias  en fila y columna

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -1,8 +1,49 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Posicion {
 	
 	private int fila;
 	private char columna;
+	
+	
+	// para evitar que si pone mayusculas tengamos que hacer mas comprobaciones lo pasamos para nosotros en minusculas todo directamente
+	// asi solo tendremos que comprobarlo una vez 
+	private char minuscula = Character.toLowerCase(columna);
+	
+    private int MAXIMO=8;
+    private int MINIMO=1;
+	
 
+
+	// Generamos los Getters y Setters
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		if (fila<MINIMO || fila> MAXIMO) {
+			
+			//si la fila esta mal marcaremos el error con el argumento 
+			throw new IllegalArgumentException("Error: La fila es eronea");
+		}else 
+		
+			this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		// comprobamos que se metan correctamente los datos
+		if (minuscula!='a' && minuscula!='b' && minuscula!='c' && minuscula!='d' && minuscula!='e' && minuscula!='f' && minuscula!='g' && minuscula!='h') {
+			throw new IllegalArgumentException("Error: La columna no es valida");
+		}
+		this.columna = columna;
+	}
 }
+	
+	
+	
+	
+	
+
