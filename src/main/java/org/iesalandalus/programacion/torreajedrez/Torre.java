3922fe212d8c3f5be386cdb0a108528ce0b6ef83
Abarlop <85289915+Abarlop@users.noreply.github.com>
2021-11-28 19:13:01 +0100

    Creacion de getter y settter en Torre [12]
    Se crean los getter y setter para los atributos

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -4,5 +4,29 @@ public class Torre {
 
 		private Color color;
 		private Posicion posicion;
+		
+		
+		
+		
+		
+		
+		
+		// Creamos los Getter y Setter [Recuerda que no aparecera la opcion si no guardas primero el archivo]
+		
+		public Color getColor() {
+			return color;
+		}
+		public void setColor(Color color) {
+			this.color = color;
+		}
+		public Posicion getPosicion() {
+			return posicion;
+		}
+		public void setPosicion(Posicion posicion) {
+			this.posicion = posicion;
+		}
+		
+		
+		
 	
 }
